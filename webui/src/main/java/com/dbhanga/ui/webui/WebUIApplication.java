713e9204fc41770854ca1058cf568be7e45b848b
Mohammad Zeyaul Haque <mhaque@ccistech.com>
2020-03-24 22:48:04 -0500

    Adding UI application with security for testing

---
+++ webui/src/main/java/com/dbhanga/ui/webui/WebUIApplication.java
@@ -0,0 +1,13 @@
+package com.dbhanga.ui.webui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebUIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebUIApplication.class, args);
+	}
+
+}
---
+++ webui/src/test/java/com/dbhanga/ui/webui/WebUIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbhanga.ui.webui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebUIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
